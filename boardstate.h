4ee5af094540e27bdc3624df70c9634802302691
abeljim <abelj1@uci.edu>
2018-01-17 18:20:02 -0800

    added boardstate.h not finished yet

---
+++ boardstate.h
@@ -0,0 +1,21 @@
+#ifndef BOARD_CONTROL_H
+#define BOARD_CONTROL_H
+
+//This is the struct that hold all the board information 
+typedef struct
+{
+    int boardarray[8][8];
+    int WKCFlag;
+    int WQCFLag;
+    int BQCFlag;
+    int BKCFLag;
+
+} BSTATE;
+
+//This will allocate the memory for the board
+BSTATE *createBstate(void);
+
+//This will load the start of the game to the inputted board
+void loadStart(BSTATE *currentboard);
+
+#endif
