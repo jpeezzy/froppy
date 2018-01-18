71d7e6e4d98b92fc95f449692d0d36ac943a0089
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-18 13:54:30 -0800

    added comments about castle flag

--- boardstate.h
+++ boardstate.h
@@ -1,24 +1,25 @@
 #ifndef BOARD_CONTROL_H
 #define BOARD_CONTROL_H
 
-//This is the struct that hold all the board information 
+// This is the struct that hold all the board information
 typedef struct
 {
-    int boardarray[8][8];
-    int WKCFlag;
-    int WQCFLag;
-    int BQCFlag;
-    int BKCFLag;
+  int boardarray[8][8];
+  // we still have the right to castle, but may not be able to castle right now
+  int WKCFlag;
+  int WQCFLag;
+  int BQCFlag;
+  int BKCFLag;
 
 } BSTATE;
 
-//This will allocate the memory for the board
+// This will allocate the memory for the board
 BSTATE *createBstate(void);
 
-//This will delete all the pointers for the board
+// This will delete all the pointers for the board
 void deleteBstate(BSTATE *board);
 
-//This will load the start of the game to the inputted board
+// This will load the start of the game to the inputted board
 void loadStart(BSTATE *currentboard);
 
 #endif
