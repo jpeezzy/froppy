f9d4e11e423709d3fbcaab04b205902047be0960
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-25 14:34:21 -0800

    update header file

--- openMP_backprop.h
+++ openMP_backprop.h
@@ -7,6 +7,13 @@ void calerrorOuputO(float* output, float* truth, float* res);
 
 // calculate derivative of error with repsect to output, take in weights and
 // derivative of error with respect to input of next layer
-void calerrorOuput(float* weight, float* errorVAl, float* res);
+void calerrorOuput(float* weight, float* nexterrorVAl, float* res);
 
+// calculate the derivative of error w.r.t to value of a node
+// parameters: the current layer value, derivative of error w.r.t output
+void calerrorVal(float* layerVal, float* errorOutput, float* res);
+
+// calculate the gradient descent
+// parameters: output of previous layer, derivative of error w.r.t value
+void calgrad(float* prevOutput, float* errorVal, float* res);
 #endif
