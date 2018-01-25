9115c39c3073830ac7ab2ecab8cb98d7824c8031
abeljim <abelj1@uci.edu>
2018-01-25 15:26:43 -0800

    started working on the nadam implmentation

--- neuralnet.c
+++ neuralnet.c
@@ -93,6 +93,31 @@ void tanhArray(float *array, int m, int n, int flag)
         }
 }
 
+//Nadam for a single part
+void nadam(float *w, float *g, float *m, float *v, int t)
+{
+    //Update the Momentum and Variance Vectors
+    *(m) = (*(m))*b1 + (1-b1)*(*(g));
+    *(v) = (*(v))*b2 + (1-b2)*(*(g))*(*(g));
+    
+    //Calculate the new weight
+    *(w) = *(w) - (eta/(sqrt((*(v))/(1-pow(b1,t))))+e)*(b1*((*(m))/(1-pow(b1,t)))+ ((1-b1)*(*(g)))/(1-pow(b1,t)));  
+}
+
+//nadam Array 
+void nadamArray( float *W, float *V, float *M, float *G, int m, int n, int t)
+{
+    int i,j;
+    
+    for(i = 0; i < m; ++i)
+    {
+        for(j = 0; j < n; ++j)
+        {
+             nadam( &(W[i*n+j]), &(V[i*n+j]), &(M[i*n+j]), &(G[i*n+j]), t);         
+        }
+    }
+}
+
 
 
 
