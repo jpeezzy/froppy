2b188b47c20f04492a9784956068d0054bb450f8
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-26 02:13:16 -0800

    added random move creation for input of auto encoder

--- autoencoder.c
+++ autoencoder.c
@@ -1,54 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include "boardToVector.h"
+#include "fentoBoardState.h"
 #include "matrix.h"
 #include "neuralnet.h"
 #include "openMP_backprop.h"
-#include "fentoBoardState.h"
-#include "boardToVector.h"
 #include "randGen.h"
 
 int main()
 {
-    //create the structs
-    AUTOW *aw, *ag, *am, *av;
-    AUTOL *al;
-    DECODEW *dw, *dg, *dm, *dv;
-    DECODEL *dl;
+  // create the structs
+  AUTOW *  aw, *ag, *am, *av;
+  AUTOL *  al;
+  DECODEW *dw, *dg, *dm, *dv;
+  DECODEL *dl;
+  BSTATE   move;
+  aw = malloc(sizeof(AUTOW));
+  al = malloc(sizeof(AUTOL));
+  dw = malloc(sizeof(DECODEW));
+  dl = malloc(sizeof(DECODEL));
+  ag = malloc(sizeof(AUTOW));
+  dg = malloc(sizeof(DECODEW));
+  am = malloc(sizeof(AUTOW));
+  av = mallox(sizeof(AUTOW));
+  dm = malloc(sizeof(DECODEW));
+  dv = malloc(sizeof(AUTOW));
 
-    aw = malloc(sizeof(AUTOW));
-    al = malloc(sizeof(AUTOL));
-    dw = malloc(sizeof(DECODEW));
-    dl = malloc(sizeof(DECODEL));
-    ag = malloc(sizeof(AUTOW));
-    dg = malloc(sizeof(DECODEW));
-    am = malloc(sizeof(AUTOW));
-    av = mallox(sizeof(AUTOW));
-    dm = malloc(sizeof(DECODEW));
-    dv = malloc(sizeof(AUTOW));
+  // Intialize the weights
+  randReluArray((float *)aw->weight0, 773, 600, 773);
+  randReluArray((float *)dw->weight3, 600, 773, 600);
 
-    //Intialize the weights 
-    randReluArray((float *) aw->weight0, 773, 600, 773);
-    randReluArray((float *) dw->weight3, 600, 773, 600);
-
-    //load the data in 
-    
-
-
-
-
-    int t = 1;
-    int i;
-    for(i=0; i<10000; ++i)
+  // load the data in
+  float vect[1][773];
+  dataB = createDataB(void);
+  readFenfile(fopen(res1.txt, 'r'), dataB);
+  move = pickRandMove(dataB);
+  boardToVector(move, (float *)vect);
+  int t = 1;
+  int i;
+  aw->input = vect;
+  for (i = 0; i < 10000; ++i)
     {
-        fowardpropAuto(aw,al,dw,dl,1);
-        backpropAuto(aw,al,dw,dl,ag,dg);
-        nadamAuto(aw,dw,ag,dg,am,av,dm,dv,t,1);
-        t=t+1;
+      fowardpropAuto(aw, al, dw, dl, 1);
+      backpropAuto(aw, al, dw, dl, ag, dg);
+      nadamAuto(aw, dw, ag, dg, am, av, dm, dv, t, 1);
+      t    = t + 1;
+      move = pickRandMove(dataB);
+      boardToVector(move, (float *)vect);
+      aw->input = vect;
     }
 
-
-    return 0;
+  return 0;
 }
