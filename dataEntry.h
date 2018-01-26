a3511cc799026d831701fc1ef923802824741ad0
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-26 01:34:34 -0800

    fixed entry header file

--- dataEntry.h
+++ dataEntry.h
@@ -7,14 +7,24 @@
 // structure used for representing an entry of a data base containing the next,
 // prev move and the state of the move it represents as well as the pt to
 // database
-typedef struct en
+typedef struct en    ENTRY;
+typedef struct datab DATABASE;
+struct en
 {
   DATABASE*  base;
   BSTATE*    state;
   struct en* next;
   struct en* prev;
-} ENTRY;
-void   readFenfile(FILE* fenFilehandle, DATABASE* dataMain);
+};
+
+struct datab
+{
+  int    totalData;
+  ENTRY* firstMove;
+  ENTRY* lastMove;
+};
+
+void   readFenfile(FILE** fenFilehandle, DATABASE* dataMain);
 BSTATE pickRandMove(DATABASE* dataMain);
 
 #endif
