babb172753ca94429b66c1a2868cccd85b31c779
khoitd1997 <khoidinhtrinh@gmail.com>
2018-01-26 02:22:52 -0800

    declare randgen

--- randGen.h
+++ randGen.h
@@ -7,16 +7,16 @@
 /* Comments: none                                                    */
 /*                                                                   */
 /*********************************************************************/
-	
+
 #ifndef RANDGEN_H
 #define RANDGEN_H
 
-uint64_t multVal =  6364136223846793005ULL;
-uint64_t seedVal =  9600629759793949339ULL;  
+uint64_t multVal = 6364136223846793005ULL;
+uint64_t seedVal = 9600629759793949339ULL;
 uint64_t incrVal = 15726070495360670683ULL;
 
 uint32_t rnd = 0;
 
-
+float randGen(void);
 
 #endif
