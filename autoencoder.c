78aafc03b4e1cd3b11974471bfe15b6a9bfac25e
abeljim <abelj1@uci.edu>
2018-01-26 00:10:46 -0800

    added traing data started autoencoder main

---
+++ autoencoder.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+#include <time.h>
+#include "matrix.h"
+#include "neuralnet.h"
+#include "openMP_backprop.h"
+#include "fentoBoardState.h"
+#include "boardToVector.h"
+#include "randGen.h"
+
+int main()
+{
+    //create the structs
+
+    //Intialize the weights 
+    
+    int i,j;
+    for(i=1; i<=4; ++i)
+    {
+        for(j=0; 
+
+    }
+
+
+    return 0;
+}
