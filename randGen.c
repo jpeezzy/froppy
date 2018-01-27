ba6ad066cf31f04e2a5e86954f59dc6f0abe6d92
jdoubleb623 <35584638+jdoubleb623@users.noreply.github.com>
2018-01-26 20:05:36 -0800

    overhauled randGen.h/.c for neuralnet compatibility

--- randGen.c
+++ randGen.c
@@ -9,46 +9,80 @@
 /*           as well as the minimal C implementation package 	     */
 /*			 provided for download via     						     */
 /*           http://www.pcg-random.org/download.html			     */
+/*      	                                                         */
+/*           edited 1/26/2018 for neuralnet compatibility (nc)       */
 /*                                                                   */
 /*********************************************************************/
 
-#include "randGen.h"
-#include <stdint.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h> /* for neuralnet compatibility testing (nct) */
+#include "randGen.h"
+
+/* global variables */
+uint64_t multVal =  6364136223846793005ULL;
+uint64_t seedVal =  9600629759793949339ULL;  
+uint64_t incrVal = 15726070495360670683ULL;
+
+uint32_t rnd = 0;
+
+/* neuralnet pseudo-global-input variables (nc) */
+int m = 8, n = 8, f = 773+600;
+float *A = NULL;
+
 
 /* ----------------------------------------------------------------- */
 /* ----------------------------------------------------------------- */
 float randGen()
 {
-  uint64_t multVal = 6364136223846793005ULL;
-  uint64_t seedVal = 9600629759793949339ULL;
-  uint64_t incrVal = 15726070495360670683ULL;
+    /* generate (sequential) seed value */
+    seedVal = seedVal > UINT64_MAX ? UINT64_MAX : seedVal+time(NULL); 
 
-  uint32_t rnd = 0;
+	/* lin congr gen (LCG) part -  var 'state' on pg 44 of PCG paper */
+	uint64_t lcgVal = multVal*seedVal + incrVal;
+	
+	/* rest of PCG_XSH_RR from minimal C implementation PCG package */
+	uint32_t xorShiftVal = ((lcgVal >> 18u) ^ lcgVal) >> 27u;
+	uint32_t rotVal = lcgVal >> 59u;
 
-  /* generate (sequential) seed value */
-  seedVal = seedVal > UINT64_MAX ? UINT64_MAX : seedVal + time(NULL);
-
-  /* lin congr gen (LCG) part -  var 'state' on pg 44 of PCG paper */
-  uint64_t lcgVal = multVal * seedVal + incrVal;
-
-  /* rest of PCG_XSH_RR from minimal C implementation PCG package */
-  uint32_t xorShiftVal = ((lcgVal >> 18u) ^ lcgVal) >> 27u;
-  uint32_t rotVal      = lcgVal >> 59u;
-
-  rnd = (xorShiftVal >> rotVal) | (xorShiftVal << ((-rotVal) & 31));
-
-  return (float)rnd / UINT32_MAX;
-
+	rnd = (xorShiftVal >> rotVal) | (xorShiftVal << ((-rotVal) & 31));
+	
+	return (float) rnd/UINT32_MAX;
+	
 } /* end uint32_t rand_PCG_XSH_RR() */
 /* ----------------------------------------------------------------- */
 /* ----------------------------------------------------------------- */
 
+/* ----------------------------------------------------------------- */
+/* ----------------------------------------------------------------- */
+void randReluArray_edited(float *A, int m, int n, int f)/* (for nct) */
+{
+	int i, j;
+	A = malloc(m*n*sizeof(float));
+	/*srand(time(NULL));*/
+	printf("\nA:\n");
+	for (i = 0; i < m; ++i)
+    {
+		for (j = 0; j < n; ++j)
+        {
+			A[i * n + j] = randGen() * (sqrt(6.0) / f);
+			/*printf("A[i*n+j] = A[%2d*%2d+%2d] = A[%2d] = %8f\n",
+			       i, n, j, i*n+j, A[i*n+j] );*/
+		    printf("%8f ", A[i*n+j]);
+        }
+		printf("\n");
+    }
+	
+	free(A); /* passes valgrind */
+	
+}
+/* ----------------------------------------------------------------- */
+/* ----------------------------------------------------------------- */
+
 /*********************************************************************/
 /*********************************************************************/
-/*
 int main() 
 {
 	int iters = 10;
@@ -57,11 +91,16 @@ int main()
 	for(int i=0; i<iters; i++)
 	{   
         printf("iter. %2d: randGen() = %0.12f\n", i+1, randGen() );
-	} 
+	}
+	randReluArray_edited(A, m, n, f);
+	
+	/*free(A); oops, wrong place */
+	
+	printf("\n");
 	
 	return 0;
 
-}*/ /* end main() */
+} /* end main() */
 /*********************************************************************/
 /*********************************************************************/
 
