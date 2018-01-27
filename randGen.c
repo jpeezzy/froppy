ff8dab65293c3256085f456de888948f7e6ebac6
jdoubleb623 <35584638+jdoubleb623@users.noreply.github.com>
2018-01-26 21:34:54 -0800

    changed spacings in Github to evade Github's default 8 indent size

--- randGen.c
+++ randGen.c
@@ -4,12 +4,12 @@
 /* Author: JAB      Date: 1/24/2018                                  */
 /*                                                                   */
 /* Comments: 100% of this code is the reimplementation of the 	     */
-/*			 PCG_XSH_RR RNG presented on pg 44 of this paper: 		 */
-/*			 http://www.pcg-random.org/pdf/hmc-cs-2014-0905.pdf      */
+/*	     PCG_XSH_RR RNG presented on pg 44 of this paper:        */
+/*	     http://www.pcg-random.org/pdf/hmc-cs-2014-0905.pdf      */
 /*           as well as the minimal C implementation package 	     */
-/*			 provided for download via     						     */
-/*           http://www.pcg-random.org/download.html			     */
-/*      	                                                         */
+/*	     provided for download via     	                     */
+/*           http://www.pcg-random.org/download.html		     */
+/*      	                                                     */
 /*           edited 1/26/2018 for neuralnet compatibility (nc)       */
 /*                                                                   */
 /*********************************************************************/
@@ -32,16 +32,16 @@ float randGen()
     /* generate (sequential) seed value */
     seedVal = seedVal > UINT64_MAX ? UINT64_MAX : seedVal+time(NULL); 
 
-	/* lin congr gen (LCG) part -  var 'state' on pg 44 of PCG paper */
-	uint64_t lcgVal = multVal*seedVal + incrVal;
+    /* lin congr gen (LCG) part -  var 'state' on pg 44 of PCG paper */
+    uint64_t lcgVal = multVal*seedVal + incrVal;
 	
-	/* rest of PCG_XSH_RR from minimal C implementation PCG package */
-	uint32_t xorShiftVal = ((lcgVal >> 18u) ^ lcgVal) >> 27u;
-	uint32_t rotVal = lcgVal >> 59u;
+    /* rest of PCG_XSH_RR from minimal C implementation PCG package */
+    uint32_t xorShiftVal = ((lcgVal >> 18u) ^ lcgVal) >> 27u;
+    uint32_t rotVal = lcgVal >> 59u;
 
-	rnd = (xorShiftVal >> rotVal) | (xorShiftVal << ((-rotVal) & 31));
+    rnd = (xorShiftVal >> rotVal) | (xorShiftVal << ((-rotVal) & 31));
 	
-	return (float) rnd/UINT32_MAX;
+    return (float) rnd/UINT32_MAX;
 	
 } /* end uint32_t rand_PCG_XSH_RR() */
 /* ----------------------------------------------------------------- */
@@ -53,16 +53,16 @@ float randGen()
 
 int main() 
 {
-	int iters = 10;
+    int iters = 10;
 	
-	printf("\n");
-	for(int i=0; i<iters; i++)
-	{   
+    printf("\n");
+    for(int i=0; i<iters; i++)
+    {   
         printf("iter. %2d: randGen() = %0.12f\n", i+1, randGen() );
-	}
-	printf("\n");
+    }
+    printf("\n");
 	
-	return 0;
+    return 0;
 
 } /* end main() */
 
