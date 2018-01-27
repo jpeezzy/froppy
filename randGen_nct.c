bf62c4cbd0543676e07e9e01e1fc1217f22c55c7
jdoubleb623 <35584638+jdoubleb623@users.noreply.github.com>
2018-01-26 21:29:40 -0800

    changed spacing in Github to evade 8 indent size

--- randGen_nct.c
+++ randGen_nct.c
@@ -4,14 +4,14 @@
 /* Author: JAB      Date: 1/24/2018                                  */
 /*                                                                   */
 /* Comments: 100% of this code is the reimplementation of the 	     */
-/*			 PCG_XSH_RR RNG presented on pg 44 of this paper: 		 */
-/*			 http://www.pcg-random.org/pdf/hmc-cs-2014-0905.pdf      */
+/*	     PCG_XSH_RR RNG presented on pg 44 of this paper:        */
+/*	     http://www.pcg-random.org/pdf/hmc-cs-2014-0905.pdf      */
 /*           as well as the minimal C implementation package 	     */
-/*			 provided for download via     						     */
-/*           http://www.pcg-random.org/download.html			     */
-/*      	                                                         */
+/*			 provided for download via                   */
+/*           http://www.pcg-random.org/download.html	             */
+/*      	                                                     */
 /*           edited 1/26/2018 for neuralnet compatibility (nc)       */
-/*           and in Github (indent size 8 to 4)                      */
+/*                                                                   */
 /*********************************************************************/
 
 #include <stdio.h>
@@ -40,16 +40,16 @@ float randGen()
     /* generate (sequential) seed value */
     seedVal = seedVal > UINT64_MAX ? UINT64_MAX : seedVal+time(NULL); 
 
-	/* lin congr gen (LCG) part -  var 'state' on pg 44 of PCG paper */
-	uint64_t lcgVal = multVal*seedVal + incrVal;
+    /* lin congr gen (LCG) part -  var 'state' on pg 44 of PCG paper */
+    uint64_t lcgVal = multVal*seedVal + incrVal;
 	
-	/* rest of PCG_XSH_RR from minimal C implementation PCG package */
-	uint32_t xorShiftVal = ((lcgVal >> 18u) ^ lcgVal) >> 27u;
-	uint32_t rotVal = lcgVal >> 59u;
+    /* rest of PCG_XSH_RR from minimal C implementation PCG package */
+    uint32_t xorShiftVal = ((lcgVal >> 18u) ^ lcgVal) >> 27u;
+    uint32_t rotVal = lcgVal >> 59u;
 
-	rnd = (xorShiftVal >> rotVal) | (xorShiftVal << ((-rotVal) & 31));
+    rnd = (xorShiftVal >> rotVal) | (xorShiftVal << ((-rotVal) & 31));
 	
-	return (float) rnd/UINT32_MAX;
+    return (float) rnd/UINT32_MAX;
 	
 } /* end uint32_t rand_PCG_XSH_RR() */
 /* ----------------------------------------------------------------- */
@@ -59,23 +59,23 @@ float randGen()
 /* ----------------------------------------------------------------- */
 void randReluArray_edited(float *A, int m, int n, int f)/* (for nct) */
 {
-	int i, j;
-	A = malloc(m*n*sizeof(float));
-	/*srand(time(NULL));*/
-	printf("\nA:\n");
-	for (i = 0; i < m; ++i)
+    int i, j;
+    A = malloc(m*n*sizeof(float));
+    /*srand(time(NULL));*/
+    printf("\nA:\n");
+    for (i = 0; i < m; ++i)
     {
-		for (j = 0; j < n; ++j)
+	for (j = 0; j < n; ++j)
         {
-			A[i * n + j] = randGen() * (sqrt(6.0) / f);
-			/*printf("A[i*n+j] = A[%2d*%2d+%2d] = A[%2d] = %8f\n",
-			       i, n, j, i*n+j, A[i*n+j] );*/
-		    printf("%8f ", A[i*n+j]);
+	    A[i * n + j] = randGen() * (sqrt(6.0) / f);
+	    /*printf("A[i*n+j] = A[%2d*%2d+%2d] = A[%2d] = %8f\n",
+		     i, n, j, i*n+j, A[i*n+j] );*/
+	    printf("%8f ", A[i*n+j]);
         }
-		printf("\n");
+	printf("\n");
     }
 	
-	free(A); /* passes valgrind */
+    free(A); /* passes valgrind */
 	
 }
 /* ----------------------------------------------------------------- */
@@ -85,20 +85,20 @@ void randReluArray_edited(float *A, int m, int n, int f)/* (for nct) */
 /*********************************************************************/
 int main() 
 {
-	int iters = 10;
+    int iters = 10;
 	
-	printf("\n");
-	for(int i=0; i<iters; i++)
-	{   
-        printf("iter. %2d: randGen() = %0.12f\n", i+1, randGen() );
-	}
-	randReluArray_edited(A, m, n, f);
+    printf("\n");
+    for(int i=0; i<iters; i++)
+    {   
+    printf("iter. %2d: randGen() = %0.12f\n", i+1, randGen() );
+    }
+    randReluArray_edited(A, m, n, f);
 	
-	/* free(A); oops, wrong place */
+    /* free(A); oops, wrong place */
 	
-	printf("\n");
+    printf("\n");
 	
-	return 0;
+    return 0;
 
 } /* end main() */
 /*********************************************************************/
